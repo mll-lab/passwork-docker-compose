0b9a0b82f2df63a2c1f0e5f62bd8c405e6cbfa8f
Benedikt Franke <benedikt.franke@mll.com>
2019-07-05 10:05:50 +0200

    Fix paths in setup script

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 set -Eeuxo pipefail
 
 # Copy the example config file of the main application
-cat sites/prod/app/config/config.example.ini | sed s/localhost:27017/mongo:27017/g > sites/prod/app/config/config.ini
+cat site/app/config/config.example.ini | sed s/localhost:27017/mongo:27017/g > site/app/config/config.ini
 
 # Initialize mongo with the latest prepared dump
-cp -r sites/prod/dump/ data/mongo/dump
+cp -r site/dump/ data/mongo/dump
 
 # Start up the stack
 docker-compose up -d
